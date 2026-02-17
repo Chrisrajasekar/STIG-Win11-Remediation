8fad113c218f7cefcb538fa0a106b07e6438d083
Christopher Rajasekar <59111838+Chrisrajasekar@users.noreply.github.com>
2026-02-16 22:47:14 -0500

    Create WN11-SO-000025.ps1

---
+++ STIGS/WN11-SO-000025.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Christopher Rajasekar
+    LinkedIn        : https://www.linkedin.com/in/christopherrajasekar/
+    GitHub          : https://github.com/Chrisrajasekar
+    Date Created    : 2026-02-15
+    Last Modified   : 2026-02-16
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-SO-000025
+
+.TESTED ON
+    Date(s) Tested  : Feb 15 2026
+    Tested By       : Christopher Rajasekar
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
